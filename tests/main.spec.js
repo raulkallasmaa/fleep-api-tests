6436493dac3f3ca147f28b6fda8925e050661b55
Marko Kreen <markokr@gmail.com>
2017-04-26 14:23:00 +0300

    initial poll completes

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -21,75 +21,57 @@ describe('test account setup', () => {
     it('should sync alice contacts', () => {
         return UC.alice.api_call("api/contact/sync/all")
             .then(res => {
-                let xres = UC.clean(res, {
-                    activity_time: null,
-                    dialog_id: ['dlg-alice', 'display_name'],
-                    account_id: ['account', 'display_name'],
-                    fleep_address: ['fladdr', 'display_name'],
-                    email: ['email', 'display_name'],
-                });
+                let xres = UC.clean(res);
                 expect(xres).toEqual({
                     "contacts": [{
                         "mk_rec_type": "contact",
                         "display_name": "Fleep Support",
                         "account_id": "<account:Fleep Support>",
                         "activity_time": '...',
-                        "dialog_id": "<dlg-alice:Fleep Support>",
+                        "dialog_id": "<dlg:Fleep Support>",
                         "email": "<email:Fleep Support>",
                         "fleep_address": "<fladdr:Fleep Support>",
                         "is_hidden_for_add": true,
                         "mk_account_status": "active",
-                        "sort_rank": 1}]
+                        "sort_rank": "..."}]
                 });
             });
     });
     it('should sync bob contacts', () => {
         return UC.bob.api_call("api/contact/sync/all")
             .then(res => {
-                let xres = UC.clean(res, {
-                    activity_time: null,
-                    dialog_id: ['dlg-bob', 'display_name'],
-                    account_id: ['account', 'display_name'],
-                    fleep_address: ['fladdr', 'display_name'],
-                    email: ['email', 'display_name'],
-                });
+                let xres = UC.clean(res);
                 expect(xres).toEqual({
                     "contacts": [{
                         "mk_rec_type": "contact",
                         "display_name": "Fleep Support",
                         "account_id": "<account:Fleep Support>",
                         "activity_time": '...',
-                        "dialog_id": "<dlg-bob:Fleep Support>",
+                        "dialog_id": "<dlg:Fleep Support>",
                         "email": "<email:Fleep Support>",
                         "fleep_address": "<fladdr:Fleep Support>",
                         "is_hidden_for_add": true,
                         "mk_account_status": "active",
-                        "sort_rank": 1}]
+                        "sort_rank": "..."}]
                 });
             });
     });
     it('should sync charlie contacts', () => {
         return UC.charlie.api_call("api/contact/sync/all")
             .then(res => {
-                let xres = UC.clean(res, {
-                    activity_time: null,
-                    dialog_id: ['dlg-charlie', 'display_name'],
-                    account_id: ['account', 'display_name'],
-                    fleep_address: ['fladdr', 'display_name'],
-                    email: ['email', 'display_name'],
-                });
+                let xres = UC.clean(res);
                 expect(xres).toEqual({
                     "contacts": [{
                         "mk_rec_type": "contact",
                         "display_name": "Fleep Support",
                         "account_id": "<account:Fleep Support>",
                         "activity_time": '...',
-                        "dialog_id": "<dlg-charlie:Fleep Support>",
+                        "dialog_id": "<dlg:Fleep Support>",
                         "email": "<email:Fleep Support>",
                         "fleep_address": "<fladdr:Fleep Support>",
                         "is_hidden_for_add": true,
                         "mk_account_status": "active",
-                        "sort_rank": 1}]
+                        "sort_rank": "..."}]
                 });
             });
     });
@@ -101,31 +83,14 @@ describe('initial poll', () => {
             .then(res => {
                 let xres = UC.clean(res, {
                     event_horizon: null,
+                    static_version: null,
                 });
+                xres.stream = [];
                 expect(xres).toEqual({
                     "event_horizon": "...",
                     "limit_time": 0,
-                    "static_version": "<jsver:->",
-                    "stream": [{
-                        "account_id": "<account:charlie>",
-                        "activated_time": "...",
-                        "client_flags": ["show_onboarding", "emoticons_old"],
-                        "connected_email": "",
-                        "display_name": "Charlie Chaplin",
-                        "email": "<email:charlie>",
-                        "export_files": [],
-                        "export_progress": "1",
-                        "fleep_address": "<fladdr:Charlie Chaplin>",
-                        "fleep_autogen": "<flautogen:Charlie Chaplin>",
-                        "has_password": true,
-                        "is_automute_enabled": true,
-                        "is_hidden_for_add": true,
-                        "is_premium": false,
-                        "mk_account_status": "active",
-                        "mk_email_interval": "never",
-                        "mk_rec_type": "contact",
-                        "trial_end_time": "...",
-                    }]
+                    "static_version": "...",
+                    "stream": []
                 });
             });
 
