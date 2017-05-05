cabdfdf873ab76d251a892aae56a26ea3ddfb251
RaulK <raul.kallasmaa@khk.ee>
2017-05-05 17:08:35 +0300

    pin, tasks and mixed keywords search

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -74,6 +74,49 @@ describe('', function () {
     });
 });
 
+describe('', function () {
+    it('pin, tasks and mixed keywords search', function () {
+        return UC.alice.api_call("api/conversation/create", {topic: 'hello friend whats up'})
+            .then(function (res) {
+                UC.clean(res, {});
+                expect(res.header.topic).toEqual('hello friend whats up');
+                return res.header.conversation_id;
+            })
+            .then(function (conversation_id) {
+                return UC.alice.api_call("api/message/store/" + conversation_id,
+                    {message: 'hello my dear friend how are you', tags: ['pin']})
+                    .then(function () {
+                        return conversation_id;
+                    });
+            })
+            .then(function (conversation_id) {
+                return UC.alice.api_call("api/message/store/" + conversation_id,
+                    {message: 'hello friend what are you doing', tags: ['is_todo']})
+                    .then(function () {
+                        return conversation_id;
+                    });
+            })
+            .then(function (conversation_id) {
+                return UC.alice.api_call("api/message/store/" + conversation_id,
+                    {message: 'hello what is going on friend', tags: ['is_done']})
+                    .then(function () {
+                        return conversation_id;
+                    });
+            })
+            .then(function (conversation_id) {
+                return promiseWait(5 * 1000, conversation_id);
+            })
+            .then(function (conversation_id) {
+                return UC.alice.api_call("api/search", {keywords: 'hello frie', search_types: ['topic', 'chat']})
+                    .then(function (res) {
+                        expect(findMsgCount(res, 'hello')).toEqual([1, 1]);
+                        return conversation_id;
+
+                    });
+            });
+    });
+});
+
 function findMsgCount(res, word) {
     let msgCount = 0;
     for (let i = 0; i < res.matches.length; i++) {
