7a29881e2f60a09e5ea01f83bcca71521c9f633a
Marko Kreen <markokr@gmail.com>
2017-05-07 14:23:37 +0300

    draft poll test

---
+++ tests/poll.spec.js
@@ -0,0 +1,43 @@
+
+import {UserCache} from '../lib';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+
+let UC = new UserCache([
+    'Alice Adamson',
+    'Bob Dylan',
+    'Charlie Chaplin',
+]);
+
+beforeAll(() => UC.setup());
+afterAll(() => UC.cleanup());
+
+describe('polling in new conversation', function () {
+    it('should send message to 3 participants', function () {
+        return Promise.all([
+            UC.alice.initial_poll(),
+            UC.bob.initial_poll(),
+            UC.charlie.initial_poll(),
+        ])
+        .then(function () {
+            return UC.alice.api_call("api/conversation/create", {
+                topic: 'test',
+                emails: [UC.bob.fleep_email, UC.charlie.fleep_email].join(', ')
+            });
+        })
+        .then(function (res) {
+            return UC.alice.api_call("api/message/send/" + res.header.conversation_id, {message: 'hello'});
+        })
+        .then(function () {
+            return Promise.all([
+                UC.alice.poll_filter({mk_rec_type: 'message', message: 'hello'}),
+                UC.bob.poll_filter({mk_rec_type: 'message', message: 'hello'}),
+                UC.charlie.poll_filter({mk_rec_type: 'message', message: 'hello'}),
+            ]);
+        })
+        .then(function (res) {
+            return true;
+        });
+    });
+});
+
