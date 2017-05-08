543a3b1bdfc0ee193e12c6fe7d01f71d1a8a8593
Marko Kreen <markokr@gmail.com>
2017-05-08 15:18:00 +0300

    imap: change test style

--- tests/imap.spec.js
+++ tests/imap.spec.js
@@ -5,38 +5,44 @@ let UC = new UserCache([ 'Box User', ]);
 beforeAll(() => UC.setup());
 afterAll(() => UC.cleanup());
 
-describe('test email sending', () => {
-    it('should send email',
-        () => UC.box.send_mail({
-            from: UC.box.email_fullname,
-            to: UC.box.email_fullname,
-            subject: 'just trying',
-            text: 'some text in body',
-        })
-        .then((res) => {
-            expect(res.accepted).toEqual([UC.box.email]);
-            return true;
-        }));
+describe('test email sending', function () {
+    it('should send email', function () {
+        return UC.box.send_mail({
+                from: UC.box.email_fullname,
+                to: UC.box.email_fullname,
+                subject: 'just trying',
+                text: 'some text in body',
+            })
+            .then(function (res) {
+                expect(res.accepted).toEqual([UC.box.email]);
+                return true;
+            });
+    });
 });
 
-describe('test email recv', () => {
-    it('should recv email',
-        () => UC.waitMail(UC.box.email)
-        .then((res) => {
-            expect(res[0].body).toEqual("some text in body");
-            return true;
-        })
-        .then(() => UC.box.send_mail({
-            from: UC.box.email_fullname,
-            to: UC.box.email_fullname,
-            subject: 'test idle',
-            text: 'another text in body',
-        }))
-        .then(() => UC.waitMail(UC.box.email))
-        .then((res) => {
-            expect(res[0].body).toEqual("another text in body");
-            return true;
-        }));
+describe('test email recv', function () {
+    it('should recv email', function () {
+        return UC.waitMail(UC.box.email)
+            .then(function (res) {
+                expect(res[0].body).toEqual("some text in body");
+                return true;
+            })
+            .then(function () {
+                return UC.box.send_mail({
+                    from: UC.box.email_fullname,
+                    to: UC.box.email_fullname,
+                    subject: 'test idle',
+                    text: 'another text in body',
+                });
+            })
+            .then(function () {
+                return UC.waitMail(UC.box.email);
+            })
+            .then(function (res) {
+                expect(res[0].body).toEqual("another text in body");
+                return true;
+            });
+    });
 });
 
 /*
