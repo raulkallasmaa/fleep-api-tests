db227d9397d1906e9346c8242f574528b28e2384
RaulK <raul.kallasmaa@khk.ee>
2017-05-08 16:33:08 +0300

    search by participants' email address

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -146,6 +146,43 @@ describe('', () => {
     );
 });
 
+describe('', () => {
+    it('should search by participants email address',
+        () => UC.alice.api_call("api/conversation/create", {})
+            .then((res) => {
+                UC.clean(res, {});
+                expect(res.header.topic).toEqual('');
+                return res.header.conversation_id;
+            })
+
+            .then((conversation_id) => UC.alice.api_call("api/conversation/add_members/" + conversation_id, {emails: [UC.bob.fleep_email, UC.charlie.fleep_email, 'tester@box.fleep.ee'].join(', ')}))
+            .then(function (res) {
+                return UC.alice.poke(res.header.conversation_id, true);
+            })
+            .then(function (conversation_id) {
+                return UC.alice.api_call("api/search", {keywords: 'tester@box.fleep.ee', search_types: ['topic']});
+            })
+            .then((res) => {
+                let xres = UC.clean(res, {});
+                xres.stream = [];
+                expect(xres).toEqual({
+                    "headers": [{
+                        "conversation_id": "<conv:Monologue with myself>",
+                        "members": ["<account:Alice Adamson>",
+                            "<account:Bob Dylan>",
+                            "<account:Charlie Chaplin>",
+                        "<account:tester@box.fleep.ee>"],
+                        "mk_rec_type": "conv",
+                        "topic": ""
+                    }],
+                    "matches": [],
+                    "stream": [],
+                    "suggestions": null
+                });
+            })
+    );
+});
+
 function findMsgCount(res, word) {
     let msgCount = 0;
     for (let i = 0; i < res.matches.length; i++) {
