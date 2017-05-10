c2136cf20122ed5a20d3260965cd5ec8275c310d
RaulK <raul.kallasmaa@khk.ee>
2017-05-10 17:57:58 +0300

    other user can delete message but not edit

--- tests/message.spec.js
+++ tests/message.spec.js
@@ -12,171 +12,212 @@ beforeAll(() => UC.setup());
 afterAll(() => UC.cleanup());
 
 // storing messages, editing them with edit and with store and adding & removing subject
-describe('store & edit messages and add & remove subject', function () {
-    it('should store & edit messages and add & remove subject', function () {
-        return UC.alice.api_call("api/conversation/create", {topic: 'greetings'})
-            .then(function (res) {
-                UC.clean(res, {});
-                expect(res.header.topic).toEqual('greetings');
-                return res.header.conversation_id;
-            })
-            .then(function (conversation_id) {
-                return UC.alice.api_call("api/conversation/add_members/" + conversation_id, {emails: [UC.bob.fleep_email, UC.charlie.fleep_email].join(', ')});
-            })
-            .then(function (res) {
-                return UC.alice.poke(res.header.conversation_id, true)
-                    .then(function () {
-                        return res.header.conversation_id;
-                    });
-            })
-            .then(function (conversation_id) {
-                return UC.alice.api_call("api/message/store/" + conversation_id, {message: 'Greetings, friend!'})
-                    .then(function () {
-                        return conversation_id;
-                    });
-            })
-            .then(function (conversation_id) {
-                return UC.alice.api_call("api/message/store/" + conversation_id, {message: 'How are you doing?'});
-            })
-            .then(function (res) {
-                return UC.alice.api_call("api/message/edit/" + res.header.conversation_id, {
-                    message: 'How are you?',
-                    message_nr: res.result_message_nr
-                })
-                    .then(function () {
-                        return UC.alice.poll_filter({
-                            mk_rec_type: 'message',
-                            message: /How are you\?/,
-                            message_nr: res.result_message_nr
-                        });
-                    })
-                    .then(function () {
-                        return res;
-                    });
-            })
-            // change message and add subject
-            .then(function (res) {
-                return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
-                    message: 'How you doing?',
-                    message_nr: res.result_message_nr,
-                    subject: 'hello there'
-                })
-                    .then(function () {
-                        return UC.alice.poll_filter({
-                            mk_rec_type: 'message',
-                            message: /How you doing\?/,
-                            message_nr: res.result_message_nr
-                        });
-                    })
-                    .then(function () {
-                        let msg = UC.alice.cache.message[res.header.conversation_id][res.result_message_nr];
-                        expect(msg.message).toEqual("<msg><p>How you doing?</p></msg>");
-                    })
-                    .then(function () {
-                        return res;
-                    });
-            })
-            // remove subject
-            .then(function (res) {
-                return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
-                    message_nr: res.result_message_nr,
-                    subject: ''
-                });
+it('should store & edit messages and add & remove subject', function () {
+    return UC.alice.api_call("api/conversation/create", {topic: 'topic1'})
+        .then(function (res) {
+            UC.clean(res, {});
+            expect(res.header.topic).toEqual('topic1');
+            return res.header.conversation_id;
+        })
+        .then(function (conversation_id) {
+            return UC.alice.api_call("api/conversation/add_members/" + conversation_id, {
+                emails: [UC.bob.fleep_email, UC.charlie.fleep_email].join(', ')
             });
-    });
+        })
+        .then(function (res) {
+            return UC.alice.poke(res.header.conversation_id, true)
+                .then(function () {
+                    return res.header.conversation_id;
+                });
+        })
+        .then(function (conversation_id) {
+            return UC.alice.api_call("api/message/store/" + conversation_id, {message: 'message1'})
+                .then(function () {
+                    return conversation_id;
+                });
+        })
+        .then(function (conversation_id) {
+            return UC.alice.api_call("api/message/store/" + conversation_id, {message: 'message2'});
+        })
+        .then(function (res) {
+            return UC.alice.api_call("api/message/edit/" + res.header.conversation_id, {
+                message: 'message3',
+                message_nr: res.result_message_nr
+            })
+                .then(function () {
+                    return UC.alice.poll_filter({
+                        mk_rec_type: 'message',
+                        message: /message3/,
+                        message_nr: res.result_message_nr
+                    });
+                })
+                .then(function () {
+                    return res;
+                });
+        })
+        // change message and add subject
+        .then(function (res) {
+            return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
+                message: 'message4',
+                message_nr: res.result_message_nr,
+                subject: 'subject1'
+            })
+                .then(function () {
+                    return UC.alice.poll_filter({
+                        mk_rec_type: 'message',
+                        message: /message4/,
+                        message_nr: res.result_message_nr
+                    });
+                })
+                .then(function () {
+                    let msg = UC.alice.cache.message[res.header.conversation_id][res.result_message_nr];
+                    expect(msg.message).toEqual("<msg><p>message4</p></msg>");
+                })
+                .then(function () {
+                    return res;
+                });
+        })
+        // remove subject and message
+        .then(function (res) {
+            return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
+                message_nr: res.result_message_nr,
+                subject: '',
+                tags: ['is_deleted']
+            });
+        });
 });
 
-describe('pin and unpin message', function () {
-    it('should pin and unpin message', function () {
-        return UC.alice.api_call("api/conversation/create", {topic: 'pin1alice'})
-            .then(function (res) {
-                UC.clean(res, {});
-                expect(res.header.topic).toEqual('pin1alice');
-                return res.header.conversation_id;
-            })
-            .then(function (conversation_id) {
-                return UC.alice.api_call("api/message/store/" + conversation_id, {
-                    message: 'pin1alice'
-                });
-            })
-            // pin message
-            .then(function (res) {
-                return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
-                    message: 'pin1alice',
-                    tags: ['pin'],
-                    message_nr: res.result_message_nr
-                })
-                    .then(function () {
-                        return res;
-                    });
-            })
-            // unpin message and change message text
-            .then(function (res) {
-                return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
-                    message_nr: res.result_message_nr,
-                    message: 'pin2',
-                    tags: ['pin', 'is_archived']
-                })
-                    .then(function (res2) {
-                        let msg = UC.alice.cache.message[res.header.conversation_id][res.result_message_nr];
-                        expect(msg.message).toEqual('<msg><p>pin2</p></msg>');
-                    });
+it('should let other user delete message but not edit & message/copy', function () {
+    return UC.alice.api_call("api/conversation/create", {topic: 'topic1'})
+        .then(function (res) {
+            UC.clean(res, {});
+            expect(res.header.topic).toEqual('topic1');
+            return res.header.conversation_id;
+        })
+        .then(function (conversation_id) {
+            return UC.alice.api_call("api/conversation/add_members/" + conversation_id, {
+                emails: [UC.bob.fleep_email, UC.charlie.fleep_email].join(', ')
             });
-    });
+        })
+        .then(function (res) {
+            return UC.alice.poke(res.header.conversation_id, true)
+                .then(function () {
+                    return res.header.conversation_id;
+                });
+        })
+        .then(function (conversation_id) {
+            return UC.alice.api_call("api/message/store/" + conversation_id, {message: 'message1'});
+        })
+        .then(function (res) {
+            return UC.bob.api_call("api/message/store/" + res.header.conversation_id, {
+                message_nr: res.result_message_nr,
+                message: 'message2'
+            })
+                .then(function () {
+                    return Promise.reject(new Error('Viga'));
+                })
+                .catch(function (r) {
+                    expect(r.statusCode).toEqual(431);
+                })
+                .then(function () {
+                    return res;
+                });
+        })
+        .then(function (res) {
+            return UC.bob.api_call("api/message/store/" + res.header.conversation_id, {
+                message_nr: res.result_message_nr,
+                tags: ['is_deleted']
+            });
+        });
 });
 
-describe('assign task, task done & undone, task archived', function () {
-    it('should assign task, set task done & undone and archive task', function () {
-        return UC.alice.api_call("api/conversation/create", {topic: 'tasks'})
-            .then(function (res) {
-                UC.clean(res, {});
-                expect(res.header.topic).toEqual('tasks');
-                return res.header.conversation_id;
-            })
-            .then(function (conversation_id) {
-                return UC.alice.api_call("api/message/store/" + conversation_id, {
-                    message: 'task1'
-                });
-            })
-            // assign task to alice
-            .then(function (res) {
-                return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
-                    message: 'task1',
-                    tags: ['is_todo'],
-                    message_nr: res.result_message_nr,
-                    assignee_ids: [UC.alice.account_id]
-                })
-                    .then(function () {
-                        return res;
-                    });
-            })
-            // task complete
-            .then(function (res) {
-                return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
-                    message_nr: res.result_message_nr,
-                    message: 'task1',
-                    tags: ['is_done']
-                });
-            })
-            // task incomplete
-            .then(function (res) {
-                return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
-                    message_nr: res.result_message_nr,
-                    message: 'task1',
-                    tags: ['is_todo']
-                });
-            })
-            // task archive
-            .then(function (res) {
-                return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
-                    message_nr: res.result_message_nr,
-                    message: 'task2',
-                    tags: ['is_todo', 'is_archived']
-                })
-                    .then(function (res2) {
-                        let msg = UC.alice.cache.message[res.header.conversation_id][res.result_message_nr];
-                        expect(msg.message).toEqual('<msg><p>task2</p></msg>');
-                    });
+it('should pin and unpin message', function () {
+    return UC.alice.api_call("api/conversation/create", {topic: 'pin1alice'})
+        .then(function (res) {
+            UC.clean(res, {});
+            expect(res.header.topic).toEqual('pin1alice');
+            return res.header.conversation_id;
+        })
+        .then(function (conversation_id) {
+            return UC.alice.api_call("api/message/store/" + conversation_id, {
+                message: 'pin1alice'
             });
-    });
+        })
+        // pin message
+        .then(function (res) {
+            return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
+                message: 'pin1alice',
+                tags: ['pin'],
+                message_nr: res.result_message_nr
+            })
+                .then(function () {
+                    return res;
+                });
+        })
+        // unpin message and change message text
+        .then(function (res) {
+            return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
+                message_nr: res.result_message_nr,
+                message: 'pin2',
+                tags: ['pin', 'is_archived']
+            })
+                .then(function (res2) {
+                    let msg = UC.alice.cache.message[res.header.conversation_id][res.result_message_nr];
+                    expect(msg.message).toEqual('<msg><p>pin2</p></msg>');
+                });
+        });
+});
+
+it('should assign task, set task done & undone and archive task', function () {
+    return UC.alice.api_call("api/conversation/create", {topic: 'tasks'})
+        .then(function (res) {
+            UC.clean(res, {});
+            expect(res.header.topic).toEqual('tasks');
+            return res.header.conversation_id;
+        })
+        .then(function (conversation_id) {
+            return UC.alice.api_call("api/message/store/" + conversation_id, {
+                message: 'task1'
+            });
+        })
+        // assign task to alice
+        .then(function (res) {
+            return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
+                message: 'task1',
+                tags: ['is_todo'],
+                message_nr: res.result_message_nr,
+                assignee_ids: [UC.alice.account_id]
+            })
+                .then(function () {
+                    return res;
+                });
+        })
+        // task complete
+        .then(function (res) {
+            return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
+                message_nr: res.result_message_nr,
+                message: 'task1',
+                tags: ['is_done']
+            });
+        })
+        // task incomplete
+        .then(function (res) {
+            return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
+                message_nr: res.result_message_nr,
+                message: 'task1',
+                tags: ['is_todo']
+            });
+        })
+        // task archive
+        .then(function (res) {
+            return UC.alice.api_call("api/message/store/" + res.header.conversation_id, {
+                message_nr: res.result_message_nr,
+                message: 'task2',
+                tags: ['is_todo', 'is_archived']
+            })
+                .then(function (res2) {
+                    let msg = UC.alice.cache.message[res.header.conversation_id][res.result_message_nr];
+                    expect(msg.message).toEqual('<msg><p>task2</p></msg>');
+                });
+        });
 });
