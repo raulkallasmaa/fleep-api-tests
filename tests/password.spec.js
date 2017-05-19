b7025b9650e3ebf7f3c002be6a1318bc3ed33ac5
RaulK <raul.kallasmaa@khk.ee>
2017-05-19 16:37:49 +0300

    reset password incomplete

--- tests/password.spec.js
+++ tests/password.spec.js
@@ -14,13 +14,21 @@ afterAll(() => UC.cleanup());
 it.skip('should reset password', function () {
    let client = UC.jessica;
    return thenSequence([
-       () => client.api_call("api/account/reset_password", {email: client.email}),
+       () => client.logout(),
+       () => client.raw_api_call("api/account/reset_password", {email: client.email}),
        () => client.waitMail({subject: /Your Fleep account access/, body: /link to reset your password/}),
        (res) => {
            let link = /https:[^\s]+/.exec(res.body)[0];
            let nfid = /notification_id=([^=&]+)/.exec(link)[1];
            //expect({link:link, nfid:nfid}).toEqual({});
+           client.password = client.password + 'esdgs53h45gh43g5k435k435dgsdf';
            return nfid;
-       }
+       },
+       (nfid) => client.raw_api_call("api/account/confirm/v2", {
+           notification_id: nfid,
+           display_name: 'Jessica Alba',
+           password: client.password,
+           fleep_address: client.info.fleep_address}),
+       () => client.login()
    ]);
 });
