83a3c2bb3423dcd3fecb43c15a938f39c8ba9516
Marko Kreen <markokr@gmail.com>
2017-05-19 15:27:07 +0300

    fix mime parsing
    - need to check text/plain
    - flatten email obj so matchStream can be used

--- tests/password.spec.js
+++ tests/password.spec.js
@@ -15,7 +15,12 @@ it.skip('should reset password', function () {
    let client = UC.jessica;
    return thenSequence([
        () => client.api_call("api/account/reset_password", {email: client.email}),
-       () => client.waitMail({}),
-       (res) => expect(res).toEqual({})
+       () => client.waitMail({subject: /Your Fleep account access/, body: /link to reset your password/}),
+       (res) => {
+           let link = /https:[^\s]+/.exec(res.body)[0];
+           let nfid = /notification_id=([^=&]+)/.exec(link)[1];
+           //expect({link:link, nfid:nfid}).toEqual({});
+           return nfid;
+       }
    ]);
 });
