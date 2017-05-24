ba7288be4dd956712993a811196e254eafaeac19
Asko Oja <ascoja@gmail.com>
2017-05-24 17:31:29 +0300

    org invit output and added reminder to testable objects

--- tests/org_invite_reminder.spec.js
+++ tests/org_invite_reminder.spec.js
@@ -22,53 +22,66 @@ let org_after_create = {
     "version_nr": 2,
 };
 
+let reminder_rec = {
+   "account_id": "<account:Theodore Bagwell>",
+   "creator_id": "<account:John Abruzzi>",
+   "expire_time": "...",
+   "is_active": true,
+   "mk_rec_type": "reminder",
+   "mk_reminder_type": "org_invite",
+   "organisation_id": "<org:organisationName>",
+   "remind_time": "...",
+   "reminder_id": "<reminder:org_invite>",
+};
+
 let sync_changelog = {
-    "stream": [{
-    "account_id": "<account:Theodore Bagwell>",
-        "event_data": {
-        "email_account_id": null,
-            "member_account_id": "<account:Theodore Bagwell>",
-    },
-    "event_time": "...",
-        "event_type": "activate_member",
-        "mk_rec_type": "org_changelog",
-        "organisation_id": "<org:organisationName>",
-        "version_nr": 3,
-},
-{
-    "account_id": "<account:John Abruzzi>",
-        "event_data": {
-        "account_id": "<account:John Abruzzi>",
-            "activate_account_ids": null,
-            "add_account_ids": [
-            "<account:Theodore Bagwell>",
-            ],
-            "add_admin_ids": null,
-            "close_account_ids": null,
-            "kick_account_ids": null,
-            "organisation_name": null,
-            "remove_account_ids": null,
-            "remove_admin_ids": null,
-            "suspend_account_ids": null,
-    },
-    "event_time": "...",
-        "event_type": "configure_org",
-        "mk_rec_type": "org_changelog",
-        "organisation_id": "<org:organisationName>",
-        "version_nr": 2,
-},
-{
-    "account_id": "<account:John Abruzzi>",
-        "event_data": {
-        "account_id": "<account:John Abruzzi>",
-            "organisation_name": "organisationName",
-    },
-    "event_time": "...",
-        "event_type": "create_org",
-        "mk_rec_type": "org_changelog",
-        "organisation_id": "<org:organisationName>",
-        "version_nr": 1,
-}]
+   "stream": [
+     {
+       "account_id": "<account:Theodore Bagwell>",
+       "event_data": {
+         "member_account_id": "<account:Theodore Bagwell>",
+       },
+       "event_time": "...",
+       "event_type": "activate_member",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:organisationName>",
+       "version_nr": 3,
+     },
+     {
+       "account_id": "<account:John Abruzzi>",
+       "event_data": {
+         "account_id": "<account:John Abruzzi>",
+         "activate_account_ids": null,
+         "add_account_ids": [
+           "<account:Theodore Bagwell>",
+         ],
+         "add_admin_ids": null,
+         "close_account_ids": null,
+         "kick_account_ids": null,
+         "organisation_name": null,
+         "remove_account_ids": null,
+         "remove_admin_ids": null,
+         "suspend_account_ids": null,
+       },
+       "event_time": "...",
+       "event_type": "configure_org",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:organisationName>",
+       "version_nr": 2,
+     },
+     {
+       "account_id": "<account:John Abruzzi>",
+       "event_data": {
+         "account_id": "<account:John Abruzzi>",
+         "organisation_name": "organisationName",
+       },
+       "event_time": "...",
+       "event_type": "create_org",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:organisationName>",
+       "version_nr": 1,
+     },
+   ],
 };
 
 test('join org via invite reminder', function () {
@@ -92,9 +105,11 @@ test('join org via invite reminder', function () {
         // get theodore into org via event reminder
         () => UC.theodore.poll_filter({mk_rec_type: 'reminder', organisation_id: client.getOrgId(org_name)}),
         () => UC.theodore.matchStream({mk_rec_type: 'reminder', organisation_id: client.getOrgId(org_name)}),
+        (res) => expect(UC.clean(res)).toEqual(reminder_rec),
+        () => UC.theodore.matchStream({mk_rec_type: 'reminder', organisation_id: client.getOrgId(org_name)}),
         (res) => UC.theodore.api_call("api/business/join/" + client.getOrgId(org_name), {
             reminder_id: res.reminder_id}),
         () => client.api_call("api/business/sync_changelog/" + client.getOrgId(/organisationName/), {}),
-        (res) => expect(UC.clean(res)).toEqual(sync_changelog)
+        (res) => expect(UC.clean(res)).toEqual(sync_changelog),
     ]);
 });
