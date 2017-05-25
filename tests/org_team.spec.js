f17c739bc1f2507841d3b1a4ea35e6c36d5b93cb
RaulK <raul.kallasmaa@khk.ee>
2017-05-25 16:08:49 +0300

    minor fixes

--- tests/org_team.spec.js
+++ tests/org_team.spec.js
@@ -15,7 +15,6 @@ let managed_team_created = {
      "<account:Charlie Chaplin>",
    ],
    "autojoin_url": "<autojoin:org-team-name>",
-   "conversations": [],
    "is_autojoin": false,
    "is_deleted": false,
    "is_managed": true,
