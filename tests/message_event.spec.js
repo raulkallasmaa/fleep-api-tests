48e3fc546ab13918a177a7865f4cdd1201ba2cc7
Kristjan Feodorov <kristjan.feodorov@gmail.com>
2017-06-02 14:19:52 +0300

    Pin & delete

--- tests/message_event.spec.js
+++ tests/message_event.spec.js
@@ -19,7 +19,7 @@ it('should store text message using stream api', function () {
         () => client.poll_filter({mk_rec_type: 'conv', topic: /topic1/}),
         () => client.api_call("api/conversation/add_members/" + client.getConvId(/topic1/), {emails: members}),
         () => client.poke(client.getConvId(/topic1/), true),
-        // store text message
+        // add text message
         () => client.api_call("api/event/store/", {
             stream: [
                 {
@@ -32,7 +32,45 @@ it('should store text message using stream api', function () {
                 },
             ],
         }),
-        // check text message
+
+        // check poll & message state
         () => client.poll_filter({mk_rec_type: 'message', message: /message1/}),
+        () => expect(client.getMessage(/message1/).mk_message_state).toEqual("urn:fleep:msgstate:text"),
+
+        // pin message
+        () => client.api_call("api/event/store/", {
+            stream: [
+                {
+                    "mk_event_type": "urn:fleep:client:conversation:message:set_pin",
+                    "client_req_id": randomUUID(),
+                    "params": {
+                        "conversation_id": client.getConvId(/topic1/),
+                        "message_nr": client.getMessageNr(/message1/),
+                    },
+                },
+            ],
+        }),
+
+        // check poll & message state
+        () => client.poll_filter({mk_rec_type: 'message', message: /message1/}),
+        () => expect(client.getMessage(/message1/).mk_message_state).toEqual("urn:fleep:msgstate:pinned"),
+
+        // del message
+        () => client.api_call("api/event/store/", {
+            stream: [
+                {
+                    "mk_event_type": "urn:fleep:client:conversation:message:del",
+                    "client_req_id": randomUUID(),
+                    "params": {
+                        "conversation_id": client.getConvId(/topic1/),
+                        "message_nr": client.getMessageNr(/message1/),
+                    },
+                },
+            ],
+        }),
+
+        // check poll & message state
+        () => client.poll_filter({mk_rec_type: 'message', message: /message1/}),
+        () => expect(client.getMessage(/message1/).mk_message_state).toEqual("urn:fleep:msgstate:deleted"),
     ]);
 });
