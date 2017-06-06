a3ce95171ac4f85f2e655737d18be8f39c405d48
Kristjan Feodorov <kristjan.feodorov@gmail.com>
2017-06-06 10:04:28 +0300

    Pinboard reordering test to try out new patterns

---
+++ tests/msgev/helpers.js
@@ -0,0 +1,94 @@
+import {thenSequence} from '../../lib';
+import {randomUUID} from '../../lib/utils';
+
+let MK_EVENT_TYPES = {
+    addPin : "urn:fleep:client:conversation:message:add_pin",
+    posPin : "urn:fleep:client:conversation:message:pos_pin",
+};
+
+let MK_MESSAGE_STATES = {
+    pinned: "urn:fleep:msgstate:pinned",
+};
+
+function setupConv(state, topic, client, members) {
+    let r_conversation = null;
+    let account_ids = [];
+    for (let member of members) {
+        account_ids.push(member.account_id);
+    }
+    return thenSequence([
+        () => client.api_call("api/conversation/create", {topic: topic, account_ids: account_ids}),
+        () => client.poll_filter({mk_rec_type: 'conv', topic: topic}),
+        () => {
+            r_conversation = client.matchStream({
+                mk_rec_type: 'conv',
+                topic: topic,
+            });
+            state.conversation_id = r_conversation.conversation_id;
+            state.client_req_id = null;
+            state.mk_event_type = null;
+            state.r_message = {};
+            state.r_request = {};
+        },
+    ]);
+}
+
+function addMessage(state, client, ev) {
+    return thenSequence([
+        () => {
+            state.client_req_id = randomUUID();
+            state.mk_event_type = ev.mk_event_type;
+        },
+        () => client.api_call("api/event/store/", {
+            stream: [
+                {
+                    "mk_event_type": state.mk_event_type,
+                    "client_req_id": state.client_req_id,
+                    "params": {
+                        "conversation_id": state.conversation_id,
+                        "message": ev.message,
+                    },
+                },
+            ],
+        }),
+        () => {
+            state.r_request = client.matchStream({
+                mk_rec_type: 'request',
+                client_req_id: state.client_req_id,
+                mk_event_type: state.mk_event_type,
+            });
+            expect(state.r_request.status_code).toEqual(200);
+            state.r_message = client.matchStream({
+                mk_rec_type: 'message',
+                conversation_id: state.r_request.identifier.conversation_id,
+                message_nr: state.r_request.identifier.message_nr,
+            });
+            expect(state.r_message.mk_message_state).toEqual(ev.mk_message_state);
+        },
+    ]);
+}
+
+function addEvent(state, client, mk_event_type, params) {
+    let client_req_id = randomUUID();
+    return thenSequence([
+        () => client.api_call("api/event/store/", {
+            stream: [
+                {
+                    "mk_event_type": mk_event_type,
+                    "client_req_id": client_req_id,
+                    "params": params,
+                },
+            ],
+        }),
+        () => {
+            state.r_request = client.matchStream({
+                mk_rec_type: 'request',
+                client_req_id: client_req_id,
+                mk_event_type: mk_event_type,
+            });
+            expect(state.r_request.status_code).toEqual(200);
+        },
+    ]);
+}
+
+export {setupConv, addMessage, addEvent, MK_EVENT_TYPES, MK_MESSAGE_STATES};
---
+++ tests/msgev/pin.spec.js
@@ -0,0 +1,110 @@
+import {UserCache, thenSequence} from '../../lib';
+import {setupConv, addEvent, MK_EVENT_TYPES, MK_MESSAGE_STATES} from './helpers';
+
+let UC = new UserCache([
+    'Alice Adamson',
+], __filename, jasmine);
+
+beforeAll(() => UC.setup());
+afterAll(() => UC.cleanup());
+
+test(`Reposition pins insice default section.`, function () {
+    let state = {};
+    return thenSequence([
+        () => setupConv(state, 'Reposition pinned messages', UC.alice, []),
+        // pin1
+        () => addEvent(state, UC.alice, MK_EVENT_TYPES.addPin, {
+            conversation_id: state.conversation_id,
+            message: "pin1",
+        }),
+        () => {
+            state.r_pin1 = UC.alice.matchStream({
+                mk_rec_type: 'message',
+                conversation_id: state.r_request.identifier.conversation_id,
+                message_nr: state.r_request.identifier.message_nr,
+            });
+            expect(state.r_pin1.mk_message_state).toEqual(MK_MESSAGE_STATES.pinned);
+        },
+        // pin2
+        () => addEvent(state, UC.alice, MK_EVENT_TYPES.addPin, {
+            conversation_id: state.conversation_id,
+            message: "pin2",
+        }),
+        () => {
+            state.r_pin2 = UC.alice.matchStream({
+                mk_rec_type: 'message',
+                conversation_id: state.r_request.identifier.conversation_id,
+                message_nr: state.r_request.identifier.message_nr,
+            });
+            expect(state.r_pin1.mk_message_state).toEqual(MK_MESSAGE_STATES.pinned);
+        },
+        // pin3
+        () => addEvent(state, UC.alice, MK_EVENT_TYPES.addPin, {
+            conversation_id: state.conversation_id,
+            message: "pin3",
+        }),
+        () => {
+            state.r_pin3 = UC.alice.matchStream({
+                mk_rec_type: 'message',
+                conversation_id: state.r_request.identifier.conversation_id,
+                message_nr: state.r_request.identifier.message_nr,
+            });
+            expect(state.r_pin1.mk_message_state).toEqual(MK_MESSAGE_STATES.pinned);
+        },
+        // pin4
+        () => addEvent(state, UC.alice, MK_EVENT_TYPES.addPin, {
+            conversation_id: state.conversation_id,
+            message: "pin4",
+        }),
+        () => {
+            state.r_pin4 = UC.alice.matchStream({
+                mk_rec_type: 'message',
+                conversation_id: state.r_request.identifier.conversation_id,
+                message_nr: state.r_request.identifier.message_nr,
+            });
+            expect(state.r_pin1.mk_message_state).toEqual(MK_MESSAGE_STATES.pinned);
+        },
+        // pins before reorder
+        () => {
+            expect([
+                { message: state.r_pin1.message, pin_weight: state.r_pin1.pin_weight },
+                { message: state.r_pin2.message, pin_weight: state.r_pin2.pin_weight },
+                { message: state.r_pin3.message, pin_weight: state.r_pin3.pin_weight },
+                { message: state.r_pin4.message, pin_weight: state.r_pin4.pin_weight },
+            ].sort((a, b) => a.pin_weight < b.pin_weight)).toEqual([
+                { message: '<msg><p>pin4</p></msg>', pin_weight: 4295163904 },
+                { message: '<msg><p>pin3</p></msg>', pin_weight: 4295098368 },
+                { message: '<msg><p>pin2</p></msg>', pin_weight: 4295032832 },
+                { message: '<msg><p>pin1</p></msg>', pin_weight: 4294967296 },
+            ]);
+        },
+        // reorder
+        () => addEvent(state, UC.alice, MK_EVENT_TYPES.posPin, {
+            "conversation_id": state.conversation_id,
+            "message_nr": state.r_pin1.message_nr,
+            "prev_message_nr": state.r_pin4.message_nr,
+        }),
+        () => {
+            state.r_pin1 = UC.alice.matchStream({
+                mk_rec_type: 'message',
+                conversation_id: state.r_request.identifier.conversation_id,
+                message_nr: state.r_request.identifier.message_nr,
+            });
+            expect(state.r_pin1.mk_message_state).toEqual(MK_MESSAGE_STATES.pinned);
+        },
+        // pins after reorder
+        () => {
+            expect([
+                { message: state.r_pin1.message, pin_weight: state.r_pin1.pin_weight },
+                { message: state.r_pin2.message, pin_weight: state.r_pin2.pin_weight },
+                { message: state.r_pin3.message, pin_weight: state.r_pin3.pin_weight },
+                { message: state.r_pin4.message, pin_weight: state.r_pin4.pin_weight },
+            ].sort((a, b) => a.pin_weight < b.pin_weight)).toEqual([
+                { message: '<msg><p>pin4</p></msg>', pin_weight: 4295163904 },
+                { message: '<msg><p>pin1</p></msg>', pin_weight: 4295131136 },
+                { message: '<msg><p>pin3</p></msg>', pin_weight: 4295098368 },
+                { message: '<msg><p>pin2</p></msg>', pin_weight: 4295032832 },
+            ]);
+        },
+    ]);
+});
