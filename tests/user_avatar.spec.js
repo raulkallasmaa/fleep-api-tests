37a30c24c1b2404fbcd7c991c60aa208b45f37cc
RaulK <raul.kallasmaa@khk.ee>
2017-06-15 16:40:03 +0300

    user avatar test complete

--- tests/user_avatar.spec.js
+++ tests/user_avatar.spec.js
@@ -24,28 +24,27 @@ test('user avatar upload, change and delete', function () {
         () => client.poll_filter({mk_rec_type: 'conv', topic: conv_topic}),
         // bob uploads a new avatar
         () => client.api_put("api/avatar/upload", './data/avatar1.jpg'),
-        // we only want to see the files array from the result and use magic on file id and upload url
+        // check that bob sees his avatar
         (res) => {
             expect(UC.clean(res.files, {
-                    file_id: ['avatar_file', 'name'],
-                    upload_url: ['upload_url', 'name']
-                })).toEqual([{
-                    "file_id": "<avatar_file:avatar1.jpg>",
-                    "file_sha256": "3508c9011a8b93ef73df7be4aa2231d2a6e7f06a9a967e18a38263cde160b281",
-                    "file_type": "image/jpeg",
-                    "height": 400,
-                    "name": "avatar1.jpg",
-                    "size": 24875,
-                    "upload_url": "<upload_url:avatar1.jpg>",
-                    "width": 400,
-                }]);
+                file_id: ['avatar_file', 'name'],
+                upload_url: ['upload_url', 'name']
+            })).toEqual([{
+                "file_id": "<avatar_file:avatar1.jpg>",
+                "file_sha256": "3508c9011a8b93ef73df7be4aa2231d2a6e7f06a9a967e18a38263cde160b281",
+                "file_type": "image/jpeg",
+                "height": 400,
+                "name": "avatar1.jpg",
+                "size": 24875,
+                "upload_url": "<upload_url:avatar1.jpg>",
+                "width": 400,
+            }]);
             let rec = client.getContact(/Bob/);
             expect(UC.clean(rec).avatar_urls).toEqual({
                 "size_100": "<avatar_size_100:Bob Marley>",
                 "size_50": "<avatar_size_50:Bob Marley>",
             });
         },
-
         // check that meg sees bobs avatar
         () => UC.meg.poll_filter({mk_rec_type: 'contact', display_name: 'Bob Marley'}),
         () => UC.meg.getRecord('contact', 'display_name', 'Bob Marley'),
@@ -57,48 +56,42 @@ test('user avatar upload, change and delete', function () {
         // bob changes his avatar
         () => client.api_put("api/avatar/upload", './data/avatar2.jpg'),
         // check that bob now has a new avatar
-        (res) => expect(UC.clean(res.files, {
-            file_id: ['avatar_file', 'name'],
-            upload_url: ['upload_url', 'name']
-        })).toEqual([{
-            "file_id": "<avatar_file:avatar2.jpg>",
-            "file_sha256": "696633432af4332e4f099466d0354c7ea7eae0c36c9f92535c855cef95b4aaa5",
-            "file_type": "image/jpeg",
-            "height": 800,
-            "name": "avatar2.jpg",
-            "size": 267164,
-            "upload_url": "<upload_url:avatar2.jpg>",
-            "width": 566,
-        }]),
+        (res) => {
+            expect(UC.clean(res.files, {
+                file_id: ['avatar_file', 'name'],
+                upload_url: ['upload_url', 'name']
+            })).toEqual([{
+                "file_id": "<avatar_file:avatar2.jpg>",
+                "file_sha256": "696633432af4332e4f099466d0354c7ea7eae0c36c9f92535c855cef95b4aaa5",
+                "file_type": "image/jpeg",
+                "height": 800,
+                "name": "avatar2.jpg",
+                "size": 267164,
+                "upload_url": "<upload_url:avatar2.jpg>",
+                "width": 566,
+            }]);
+            let rec = client.getContact(/Bob/);
+            expect(UC.clean(rec).avatar_urls).toEqual({
+                "size_100": "<avatar_size_100:Bob Marley>",
+                "size_50": "<avatar_size_50:Bob Marley>",
+                "size_575": "<avatar_size_575:Bob Marley>",
+            });
+        },
+        // check that meg sees bobs new avatar
+        () => UC.meg.poll_filter({mk_rec_type: 'contact', display_name: 'Bob Marley'}),
+        () => UC.meg.getRecord('contact', 'display_name', 'Bob Marley'),
+        (res) => expect(UC.clean(res).avatar_urls).toEqual({
+            "size_100": "<avatar_size_100:Bob Marley>",
+            "size_50": "<avatar_size_50:Bob Marley>",
+            "size_575": "<avatar_size_575:Bob Marley>",
+        }),
+
         // bob deletes his avatar and now there's no avatar connected to his contact record
         () => client.api_call("api/avatar/delete", {}),
-        (res) => expect(UC.clean(res)).toEqual({
-            "stream": [{
-            "account_id": "<account:Bob Marley>",
-            "activated_time": "...",
-            "avatar_urls": {},
-            "client_flags": [
-            "emoticons_old",
-            "show_onboarding",
-            ],
-            "connected_email": "",
-            "dialog_id": null,
-            "display_name": "Bob Marley",
-            "email": "<email:Bob Marley>",
-            "export_files": [],
-            "export_progress": "1",
-            "fleep_address": "<fladdr:Bob Marley>",
-            "fleep_autogen": "<flautogen:Bob Marley>",
-            "has_password": true,
-            "is_automute_enabled": true,
-            "is_hidden_for_add": true,
-            "is_premium": false,
-            "mk_account_status": "active",
-            "mk_email_interval": "never",
-            "mk_rec_type": "contact",
-            "organisation_id": null,
-            "trial_end_time": "...",
-            }]
-        }),
+        (res) => expect(UC.clean(res).stream[0].avatar_urls).toEqual({}),
+        // meg checks that bob has no avatar
+        () => UC.meg.poll_filter({mk_rec_type: 'contact', display_name: 'Bob Marley'}),
+        () => UC.meg.getRecord('contact', 'display_name', 'Bob Marley'),
+        (res) => expect(UC.clean(res).avatar_urls).toEqual({}),
     ]);
 });
