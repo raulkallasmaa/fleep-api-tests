71180c4e0f2f0c71b92d56a7e1c7352df4ceda62
Asko Oja <ascoja@gmail.com>
2017-06-15 21:09:57 +0300

    managed conv test now tests also changelog events

--- tests/org_managed_conv.spec.js
+++ tests/org_managed_conv.spec.js
@@ -43,6 +43,99 @@ let create_conv_result = {
    ],
 };
 
+let sync_changelog = {
+   "stream": [
+     {
+       "account_id": "<account:Charlie Chaplin>",
+       "event_data": {
+         "account_id": "<account:Charlie Chaplin>",
+         "admin_ids": [
+           "<account:Don Johnson>",
+           "<account:Mel Gibson>",
+         ],
+         "conversation_id": "<conv:managedConvTopic>",
+         "topic": "Charlie, Don and Mel",
+       },
+       "event_time": "...",
+       "event_type": "chat.set_admins",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:managedConvOrgName>",
+       "version_nr": 6,
+     },
+     {
+       "account_id": "<account:Charlie Chaplin>",
+       "event_data": {
+         "account_id": "<account:Charlie Chaplin>",
+         "conversation_id": "<conv:managedConvTopic>",
+         "topic": "Charlie, Don and Mel",
+       },
+       "event_time": "...",
+       "event_type": "chat.set_managed",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:managedConvOrgName>",
+       "version_nr": 5,
+     },
+     {
+       "account_id": "<account:Charlie Chaplin>",
+       "event_data": {
+         "account_id": "<account:Charlie Chaplin>",
+         "conversation_id": "<conv:managedConvTopic>",
+         "topic": "managedConvTopic",
+       },
+       "event_time": "...",
+       "event_type": "chat.set_unmanaged",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:managedConvOrgName>",
+       "version_nr": 4,
+     },
+     {
+       "account_id": "<account:Charlie Chaplin>",
+       "event_data": {
+         "account_id": "<account:Charlie Chaplin>",
+         "admin_ids": [
+           "<account:Don Johnson>",
+           "<account:Mel Gibson>",
+         ],
+         "conversation_id": "<conv:managedConvTopic>",
+         "topic": "managedConvTopic",
+       },
+       "event_time": "...",
+       "event_type": "chat.set_admins",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:managedConvOrgName>",
+       "version_nr": 3,
+     },
+     {
+       "account_id": "<account:Charlie Chaplin>",
+       "event_data": {
+         "account_id": "<account:Charlie Chaplin>",
+         "conversation_id": "<conv:managedConvTopic>",
+         "member_ids": [
+           "<account:Charlie Chaplin>",
+         ],
+         "topic": "managedConvTopic",
+       },
+       "event_time": "...",
+       "event_type": "chat.create_conversation",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:managedConvOrgName>",
+       "version_nr": 2,
+     },
+     {
+       "account_id": "<account:Charlie Chaplin>",
+       "event_data": {
+         "account_id": "<account:Charlie Chaplin>",
+         "organisation_name": "managedConvOrgName",
+       },
+       "event_time": "...",
+       "event_type": "create_org",
+       "mk_rec_type": "org_changelog",
+       "organisation_id": "<org:managedConvOrgName>",
+       "version_nr": 1,
+     },
+   ],
+};
+
 test('create org and create managed conv with team in it', function () {
     let client = UC.charlie;
     let conv_topic = 'managedConvTopic';
@@ -62,5 +155,28 @@ test('create org and create managed conv with team in it', function () {
             topic: conv_topic,
             team_ids: [client.getTeamId(org_team)], }),
         (res) => expect(UC.clean(res)).toEqual(create_conv_result),
+        () => client.poll_filter({mk_rec_type: 'conv', topic: conv_topic}),
+        // set admins
+        () => client.api_call("api/business/store_conversation/" + client.getOrgId(org_name), {
+            conversation_id: client.getConvId(conv_topic),
+            admins: [UC.mel.account_id, UC.don.account_id]}),
+        // set unmanaged
+        () => client.api_call("api/business/store_conversation/" + client.getOrgId(org_name), {
+            conversation_id: client.getConvId(conv_topic),
+            is_managed: false}),
+        // sremove topic to see default topic in changelog
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+            topic: ''}),
+	// set managed
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+            is_managed: true}),
+	// set admins via conversation store
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+             admins: [UC.mel.account_id, UC.don.account_id]}),
+        // let bg worker do it's thing
+        () => client.poke(client.getConvId(conv_topic), true),
+        // check changelog messages
+        () => client.api_call("api/business/sync_changelog/" + client.getOrgId(org_name)),
+        (res) => expect(UC.clean(res)).toEqual(sync_changelog),
     ]);
 });
