49498d85c517f33a7756f0579eee634090e22007
RaulK <raul.kallasmaa@khk.ee>
2017-08-18 16:35:59 +0300

    invite fleep user to org and check that they get an email

---
+++ tests/org_invite_fleeper.spec.js
@@ -0,0 +1,35 @@
+import {UserCache, thenSequence} from '../lib';
+
+let UC = new UserCache([
+    'Bob Dylan',
+    'Ben Dover',
+], __filename, jasmine);
+
+beforeAll(() => UC.setup());
+afterAll(() => UC.cleanup());
+
+test('invite fleep user to org and check that they get an email', function () {
+    let client = UC.bob;
+    let org_name = 'organisationName';
+
+    return thenSequence([
+        () => UC.ben.login(),
+        // create org
+        () => client.api_call("api/business/create", {organisation_name: org_name}),
+        () => client.poll_filter({mk_rec_type: 'org_header', organisation_name: org_name}),
+
+        // do a lookup for the fleep user ben
+        () => client.api_call("api/account/lookup", {lookup_list: [UC.ben.email], ignore_list: []}),
+
+        // invite fleep user ben to org
+        () => client.api_call("api/business/configure/" + client.getOrgId(org_name), {
+            add_account_ids: [
+                client.getRecord('contact', 'email', UC.ben.email).account_id]
+        }),
+        // ben gets an email about the invite to the org
+        () => UC.ben.waitMail({
+            subject: /Invitation to/,
+            body: /invites you to join the organization/
+        }),
+    ]);
+});
