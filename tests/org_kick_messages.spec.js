5ac1246e4c36bb34de1c73cee8526fc7aed9389d
RaulK <raul.kallasmaa@khk.ee>
2017-10-03 00:27:05 +0300

    user has been in the conv earlier and is added back, gets kicked and sees earlier content and kick message

--- tests/org_kick_messages.spec.js
+++ tests/org_kick_messages.spec.js
@@ -136,3 +136,78 @@ test('user is member of the convo when it becomes managed, gets kicked and sees 
         }),
     ]);
 });
+
+test('user has been in the conv earlier and is added back, gets kicked and sees earlier content and kick message', function () {
+    let client = UC.jon;
+    let conv_topic = 'earlyContentKickMessage';
+    let org_name = 'orgName3';
+
+    return thenSequence([
+        () => UC.mel.initial_poll(),
+        // create org
+        () => client.api_call("api/business/create", {organisation_name: org_name}),
+        () => client.poll_filter({mk_rec_type: 'org_header', organisation_name: org_name}),
+        // create conv and add mel
+        () => client.api_call("api/conversation/create", {
+            topic: conv_topic,
+            account_ids: [UC.mel.account_id],
+        }),
+        () => client.poll_filter({mk_rec_type: 'conv', topic: conv_topic}),
+        // send a few messages to flow
+        () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'message7'}),
+        () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'message8'}),
+        // check that jon can't kick mel from a regular conv
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+            kick_account_ids: [UC.mel.account_id],
+        })
+            .then(() => Promise.reject(new Error('Can not kick from an unmanaged conv!')),
+                (r) => expect(r.statusCode).toEqual(431)),
+        // mel leaves the conv
+        () => UC.mel.api_call("api/conversation/leave/" + client.getConvId(conv_topic), {}),
+        // send a message to flow after mel is gone
+        () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'message9'}),
+        // add mel back to the conv
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+            add_account_ids: [UC.mel.account_id],
+        }),
+        () => client.poke(client.getConvId(conv_topic), true),
+        () => UC.mel.poke(client.getConvId(conv_topic), true),
+        // turn the conv to managed conv
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+            is_managed: true,
+        }),
+        // kick mel from the conv
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+            kick_account_ids: [UC.mel.account_id],
+        }),
+        () => UC.mel.poke(client.getConvId(conv_topic), true),
+        () => UC.mel.api_call("api/conversation/sync/" + client.getConvId(conv_topic), {}),
+        // check that mel can see the first 2 messages but not the 3rd
+        () => expect(UC.clean(UC.mel.getMessage(/message7/)).message).toEqual("<msg><p>message7</p></msg>"),
+        () => expect(UC.clean(UC.mel.getMessage(/message8/)).message).toEqual("<msg><p>message8</p></msg>"),
+        () => expect(UC.clean(UC.mel.matchStream({mk_rec_type: 'message', message: /message9/}))).toEqual(null),
+        // check that mel sees the kick message
+        () => expect(UC.clean(UC.mel.getMessage(/administrator/))).toEqual({
+            "account_id": "<account:Jon Snow>",
+            "conversation_id": "<conv:earlyContentKickMessage>",
+            "inbox_nr": -3,
+            "lock_account_id": null,
+            "message": {
+            "members": [
+            "<account:Mel Gibson>",
+            ],
+            "org_name": "orgName3",
+            "organisation_id": "<org:orgName3>",
+            "sysmsg_text": "An administrator of {org_name} removed {members} and their access to the conversation's content.",
+            },
+            "message_nr": 8,
+            "mk_message_state": "urn:fleep:message:mk_message_state:system",
+            "mk_message_type": "kickV2",
+            "mk_rec_type": "message",
+            "posted_time": "...",
+            "prev_message_nr": 7,
+            "profile_id": "<account:Mel Gibson>",
+            "tags": [],
+        }),
+    ]);
+});
