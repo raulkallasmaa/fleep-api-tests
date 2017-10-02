a364692eff390150e28bc873605519e8976afa52
RaulK <raul.kallasmaa@khk.ee>
2017-10-02 22:36:16 +0300

    user is member of the convo when it becomes managed, gets kicked and sees earlier content and kick message

--- tests/org_kick_messages.spec.js
+++ tests/org_kick_messages.spec.js
@@ -3,7 +3,9 @@ import {UserCache, thenSequence} from '../lib';
 let UC = new UserCache([
     'Bob Marley',
     'Meg Griffin',
+    'Ben Dover',
     'Don Johnson',
+    'Jon Snow',
     'Mel Gibson',
 ], __filename, jasmine);
 
@@ -67,6 +69,69 @@ test('user is added to managed conv and kicked & only sees the kick message', fu
             "posted_time": "...",
             "prev_message_nr": 5,
             "profile_id": "<account:Meg Griffin>",
+            "tags": [],
+        }),
+    ]);
+});
+
+test('user is member of the convo when it becomes managed, gets kicked and sees earlier content and kick message', function () {
+    let client = UC.ben;
+    let conv_topic = 'earlierContentAndKickMessage';
+    let org_name = 'orgName2';
+
+    return thenSequence([
+        () => UC.don.initial_poll(),
+        // create org
+        () => client.api_call("api/business/create", {organisation_name: org_name}),
+        () => client.poll_filter({mk_rec_type: 'org_header', organisation_name: org_name}),
+        // create conv and add don
+        () => client.api_call("api/conversation/create", {
+            topic: conv_topic,
+            account_ids: [UC.don.account_id],
+        }),
+        () => client.poll_filter({mk_rec_type: 'conv', topic: conv_topic}),
+        // send a few messages to flow
+        () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'message4'}),
+        () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'message5'}),
+        () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'message6'}),
+        // turn the conv to managed conv
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+            is_managed: true,
+        }),
+        () => client.poke(client.getConvId(conv_topic), true),
+        // check that the conv is managed
+        () => expect(UC.clean(client.getConv(conv_topic)).is_managed).toEqual(true),
+        // kick don from the conv
+        () => client.api_call("api/conversation/store/" + client.getConvId(conv_topic), {
+            kick_account_ids: [UC.don.account_id],
+        }),
+        () => UC.don.poke(client.getConvId(conv_topic), true),
+        () => UC.don.api_call("api/conversation/sync/" + client.getConvId(conv_topic), {}),
+        // check that don can see the 3 messages
+        () => expect(UC.clean(UC.don.getMessage(/message4/)).message).toEqual("<msg><p>message4</p></msg>"),
+        () => expect(UC.clean(UC.don.getMessage(/message5/)).message).toEqual("<msg><p>message5</p></msg>"),
+        () => expect(UC.clean(UC.don.getMessage(/message6/)).message).toEqual("<msg><p>message6</p></msg>"),
+        // check that don sees the kick message
+        () => expect(UC.clean(UC.don.getMessage(/administrator/))).toEqual({
+            "account_id": "<account:Ben Dover>",
+            "conversation_id": "<conv:earlierContentAndKickMessage>",
+            "inbox_nr": -3,
+            "lock_account_id": null,
+            "message": {
+            "members": [
+            "<account:Don Johnson>",
+            ],
+            "org_name": "orgName2",
+            "organisation_id": "<org:orgName2>",
+            "sysmsg_text": "An administrator of {org_name} removed {members} and their access to the conversation's content.",
+            },
+            "message_nr": 6,
+            "mk_message_state": "urn:fleep:message:mk_message_state:system",
+            "mk_message_type": "kickV2",
+            "mk_rec_type": "message",
+            "posted_time": "...",
+            "prev_message_nr": 5,
+            "profile_id": "<account:Don Johnson>",
             "tags": [],
         }),
     ]);
