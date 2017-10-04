249eb848dac3257b0a15637d10c3eff19569803c
Marko Kreen <markokr@gmail.com>
2017-10-04 13:48:34 +0300

    http: warn about blog version

--- tests/http.test.js
+++ tests/http.test.js
@@ -25,18 +25,21 @@ function mkheaders(ctype, base) {
 }
 
 
-let DROP_HTML = ['content-length', 'content-security-policy', 'date', 'etag', 'last-modified', 'server', 'connection'];
+let DROP_HTML = ['content-length', 'content-security-policy', 'date', 'etag', 'last-modified', 'connection'];
 
-let DROP_STATIC = ['content-length', 'date', 'etag', 'last-modified', 'server', 'connection',
+let DROP_STATIC = ['content-length', 'date', 'etag', 'last-modified', 'connection',
     // fixme: test caching
     'cache-control', 'expires'];
+
+let DROP_BLOG = ['date', 'content-security-policy', 'connection'];
 
 
 let COMMON = {
     "strict-transport-security": "max-age=31536000; includeSubDomains",
     "x-content-type-options": "nosniff",
     "x-frame-options": "deny",
-    "x-xss-protection": "1; mode=block"
+    "x-xss-protection": "1; mode=block",
+    "server": "nginx",
 };
 
 let GZIP = {
@@ -66,6 +69,17 @@ let STATIC_COMMON = Object.assign({}, COMMON, {
 let STATIC_NOGZ = Object.assign({}, STATIC_COMMON, NOGZ);
 let STATIC_GZ = Object.assign({}, STATIC_COMMON, GZIP);
 
+
+let BLOG_PAGE = {
+    'strict-transport-security': 'max-age=31536000; includeSubDomains; preload',
+    'x-frame-options': 'SAMEORIGIN',
+    'server': 'nginx',
+    'x-content-type-options': 'nosniff',
+    'transfer-encoding': 'chunked', // ?? FIXME
+    // cache-control?
+    // vary
+    // x-xss-protection
+};
 
 test('http headers', function () {
     let client = UC.demo;
@@ -104,6 +118,40 @@ test('http headers', function () {
         (res) => expect(cleanHeaders(res, DROP_STATIC)).toEqual(mkheaders('application/javascript; charset=utf-8', STATIC_GZ)),
         () => client.raw_request('/v/xxx/assets/fleep/common.js'),
         (res) => expect(cleanHeaders(res, DROP_STATIC)).toEqual(mkheaders('application/javascript; charset=utf-8', STATIC_GZ)),
+        () => client.raw_request('/v/xxx/assets/fleep/common.js'),
+        (res) => expect(cleanHeaders(res, DROP_STATIC)).toEqual(mkheaders('application/javascript; charset=utf-8', STATIC_GZ)),
+    ]);
+});
+
+test('blog headers', function () {
+    let client = UC.demo;
+    let wpver, latest;
+
+    return thenSequence([
+        () => client.raw_request('blog/'),
+        (res) => {
+            expect(cleanHeaders(res, DROP_BLOG)).toEqual(mkheaders('text/html; charset=UTF-8', BLOG_PAGE));
+            let m = /<meta name="generator" content="([^"]*)"/.exec(res.body);
+            wpver = m ? m[1] : "<unknown>";
+            expect(wpver).toMatch(/^WordPress /);
+            wpver = wpver.split(' ')[1];
+        },
+
+        () => client.raw_request('-', {uri: 'https://wordpress.org/download/release-archive/'}),
+        (res) => {
+            expect(res.statusCode).toEqual(200);
+            let parts = wpver.split('.');
+            let mrx = new RegExp('<td>(' + parts[0] + '[.]' + parts[1] + '[.][^<]+)</td>', 'g');
+            let m = mrx.exec(res.body);
+            latest = m ? m[1] : '<failed>';
+
+            //expect(latest).toEqual(wpver);
+            if (latest !== wpver) {
+                let m2 = /<th>Version<\/th>[\s\S]*?<td>([0-9]\.[0-9.]+)<\/td>/m.exec(res.body);
+                let wplatest = m2 ? m2[1] : "<not found>";
+                console.warn("Blog is not using latest wordpress: installed: " + wpver + " latest: " + latest + ' (head: ' + wplatest + ')');
+            }
+        },
     ]);
 });
 
