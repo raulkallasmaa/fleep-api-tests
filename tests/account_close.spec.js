f8bf573973bbf6a4412bb29d32ba80e9e9383d5c
RaulK <raul.kallasmaa@khk.ee>
2017-10-20 15:32:16 +0300

    account close test with additions on skip right now until bug fixed

--- tests/account_close.spec.js
+++ tests/account_close.spec.js
@@ -3,12 +3,13 @@ import {UserCache, thenSequence} from '../lib';
 let UC = new UserCache([
     'Bob Marley',
     'Meg Griffin',
+    'Jon Lajoie',
 ], __filename, jasmine);
 
 beforeAll(() => UC.setup());
 afterAll(() => UC.cleanup());
 
-test('account close tests', function () {
+test.skip('account close test', function () {
     let client = UC.bob;
     let conv_topic = 'topic1';
     let conv_topic2 = 'topic2';
@@ -59,7 +60,48 @@ test('account close tests', function () {
         () => UC.meg.login()
             .then(() => Promise.reject(new Error('Incorrect email or password')),
                 (r) => expect(r.statusCode).toEqual(431)),
-        // THIS API CALL SHOULDN'T WORK. ORG ADMIN SHOULDN'T BE ABLE TO CLOSE HIS ACCOUNT
+        // do a lookup for jon
+        () => client.api_call("api/account/lookup", {lookup_list: [UC.jon.email], ignore_list: []}),
+        // invite fleep user jon to the org
+        () => client.api_call("api/business/configure/" + client.getOrgId(org_name), {
+            add_account_ids: [UC.jon.account_id]
+        }),
+        // jon joins the org
+        () => UC.jon.poll_filter({mk_rec_type: 'reminder', organisation_id: client.getOrgId(org_name)}),
+        () => UC.jon.matchStream({mk_rec_type: 'reminder', organisation_id: client.getOrgId(org_name)}),
+        (res) => UC.jon.api_call("api/business/join/" + client.getOrgId(org_name), {reminder_id: res.reminder_id}),
+        () => client.api_call("api/business/sync/" + client.getOrgId(org_name)),
+        // check that jon is an active member of the org
+        () => expect(UC.clean(client.getRecord('org_member', 'account_id', UC.jon.account_id))).toEqual({
+            "account_id": "<account:Jon Lajoie>",
+            "inviter_id": "<account:Bob Marley>",
+            "is_admin": false,
+            "mk_member_status": "bms_active",
+            "mk_rec_type": "org_member",
+            "organisation_id": "<org:orgName1>",
+        }),
+        // jon tries to close his account but can't
+        () => UC.jon.api_call("api/account/close")
+            .then(() => Promise.reject(new Error('Org members can be closed only by org admins')),
+                (r) => expect(r.statusCode).toEqual(430)),
+        // !!! org admin bob shouldn't be able to close his account while he's not the only member left in the org, needs a fix !!!
         () => client.api_call("api/account/close"),
+        /*
+        () => UC.jon.api_call("api/business/sync/" + client.getOrgId(org_name)),
+        () => expect(UC.clean(UC.jon.getOrg(org_name))).toEqual({
+            "active_member_count": "...",
+            "grace_time": "...",
+            "is_admin": false,
+            "is_member": true,
+            "mk_rec_type": "org_header",
+            "organisation_founder_id": "<account:Bob Marley>",
+            "organisation_id": "<org:orgName1>",
+            "organisation_name": "orgName1",
+            "status": "bos_closed",
+            "trial_time": "...",
+            "version_nr": 4,
+        }),
+        () => expect(UC.clean(UC.jon.getOrg(org_name).active_member_count)).toEqual(1),
+        */
     ]);
 });
