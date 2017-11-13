76ec6ed2230294178c850492085f7004ff320932
Asko Oja <ascoja@gmail.com>
2017-11-13 10:11:19 +0200

    stabilisation

--- tests/disclose_team_ids.spec.js
+++ tests/disclose_team_ids.spec.js
@@ -565,6 +565,7 @@ test('conversation disclose and disclose all in free conversation and free team 
         // create a free conversation and send 2 messages
         () => client.api_call("api/conversation/create", {topic: conv_topic}),
         () => client.poll_filter({mk_rec_type: 'conv', topic: conv_topic}),
+        () => client.poke(client.getConvId(conv_topic), true),
         () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'msg1'}),
         () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'msg2'}),
         // add team1 to the conv
