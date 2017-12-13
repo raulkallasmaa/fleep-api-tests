3ce0aa85022e2e137dd2274670d2ac03fc8ea8d5
Kristjan Feodorov <kristjan.feodorov@gmail.com>
2017-12-13 15:06:34 +0200

    Test prop

--- tests/msgev/mytask.spec.js
+++ tests/msgev/mytask.spec.js
@@ -54,6 +54,7 @@ test(`Test my taskboard sync`, function () {
                 mk_message_state: ms.TODO,
                 message: '<msg><p>TodoMessage1</p></msg>',
             });
+            //console.log(state.r_message1);
         },
         // wait for event sender to process the message
         () => UC.alice.poll_filter({mk_rec_type: 'message', message_nr: state.r_message1.message_nr}),
@@ -70,6 +71,74 @@ test(`Test my taskboard sync`, function () {
             expect(state.r_message_my_task).toMatchObject({
                 mk_message_state: ms.TODO,
                 message: '<msg><p>TodoMessage1</p></msg>',
+            });
+            expect(UC.clean(state.r_message_my_task.task_state)).toMatchObject({
+                mk_section_type: 'urn:fleep:section:mk_section_type:my_task',
+                section_id: '<section:To Do>',
+                task_weight: 4294967296,
+            });
+        },
+    ]);
+});
+
+test(`Test my taskboard info propagation`, function () {
+    let state = {};
+    return thenSequence([
+        // do initial poll
+        () => UC.alice.initial_poll(),
+        // check my taskboard default section
+        () => {
+            state.r_section_default = UC.alice.matchStream({
+                mk_rec_type: 'section',
+                mk_section_type: 'urn:fleep:section:mk_section_type:my_task',
+                mk_section_subtype: 'urn:fleep:section:mk_section_subtype:default',
+            });
+            expect(UC.clean(state.r_section_default)).toMatchObject({
+               section_id: '<section:To Do>',
+            });
+        },
+        // check my taskboard archived section
+        () => {
+            state.r_section_archived = UC.alice.matchStream({
+                mk_rec_type: 'section',
+                mk_section_type: 'urn:fleep:section:mk_section_type:my_task',
+                mk_section_subtype: 'urn:fleep:section:mk_section_subtype:archived',
+            });
+            expect(UC.clean(state.r_section_archived)).toMatchObject({
+               section_id: '<section:Archived>',
+            });
+        },
+        () => setupConv(state, 'Create task', UC.alice, [UC.bob]),
+        // bob assignes task to alice
+        () => addEvent(state, UC.bob, et.MESSAGE_ADD__TODO, {
+            conversation_id: state.conversation_id,
+            message: "Message assigned to alice",
+            assignee_ids: [UC.alice.account_id],
+        }),
+
+        () => {
+            state.r_message1 = UC.bob.matchStream({
+                mk_rec_type: 'message',
+                conversation_id: state.r_request.identifier.conversation_id,
+                message_nr: state.r_request.identifier.message_nr,
+            });
+            expect(state.r_message1).toMatchObject({
+                mk_message_state: ms.TODO,
+                message: '<msg><p>Message assigned to alice</p></msg>',
+            });
+        },
+        // alice waits for the message to arrive with poll
+        () => UC.alice.poll_filter({mk_rec_type: 'message', message_nr: state.r_message1.message_nr}),
+        () => {
+            state.r_message_my_task = UC.alice.matchStream({
+                mk_rec_type: 'message',
+                conversation_id: state.r_message1.conversation_id,
+                message_nr: state.r_message1.message_nr,
+            });
+            console.log(state.r_message_my_task);
+            expect(state.r_message_my_task).toMatchObject({
+                mk_message_state: ms.TODO,
+                message: '<msg><p>Message assigned to alice</p></msg>',
             });
             expect(UC.clean(state.r_message_my_task.task_state)).toMatchObject({
                 mk_section_type: 'urn:fleep:section:mk_section_type:my_task',
