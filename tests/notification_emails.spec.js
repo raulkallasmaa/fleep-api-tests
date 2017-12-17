b495b0d8e3bca613fe9fbbeba6b24b1bfeef8298
RaulK <raul.kallasmaa@khk.ee>
2017-12-18 00:55:25 +0200

    send some messages and wait for corresponding notification emails

--- tests/notification_emails.spec.js
+++ tests/notification_emails.spec.js
@@ -1,4 +1,5 @@
 import {UserCache, thenSequence} from '../lib';
+import {waitAsync} from "../lib/utils";
 
 let UC = new UserCache([
     'Bob Marley',
@@ -9,29 +10,52 @@ let UC = new UserCache([
 beforeAll(() => UC.setup());
 afterAll(() => UC.cleanup());
 
-test.skip('send a message and wait for corresponding notification email', function () {
+test('send some messages and wait for corresponding notification emails', function () {
     let client = UC.bob;
-    let conv_topic = 'notificationEmail';
+    let msg160 = "1234567890".repeat(16);
+    let topic60 = "0987654321".repeat(6);
 
     return thenSequence([
-        () => UC.meg.api_call("api/account/configure", {email_interval: 'daily'}),
-        () => UC.meg.api_call("api/account/configure", {is_automute_enabled: false}),
+        // set megs email interval to hourly and mark fleep support conv as read so she doesn't receive an email about that
+        () => UC.meg.api_call("api/account/configure", {email_interval: 'hourly'}),
+        () => UC.meg.poll_filter({mk_rec_type: 'conv', default_topic: 'Fleep Support'}),
+        () => UC.meg.getRecord('conv', 'default_topic', 'Fleep Support'),
+        (res) => UC.meg.api_call("api/conversation/mark_read/" + res.conversation_id, {}),
         // create a conv with fleep user meg
-        () => client.api_call("api/conversation/create", {
-            topic: conv_topic,
-            account_ids: [UC.meg.account_id],
-        }),
-        () => client.poll_filter({mk_rec_type: 'conv', topic: conv_topic}),
-        // send meg a message
-        () => client.api_call("api/message/store/" + client.getConvId(conv_topic), {message: 'Message'}),
-        () => UC.meg.poke(client.getConvId(conv_topic), true),
-        // turn meg inactive for 7 days and look for notification email
+        () => client.api_call("api/conversation/create", {account_ids: [UC.meg.account_id]}),
+        () => client.poll_filter({mk_rec_type: 'conv', topic: ''}),
+        // send meg a message that has 160 characters and a random message
+        () => client.api_call("api/message/store/" + client.getConvId(''), {message: msg160}),
+        () => client.api_call("api/message/store/" + client.getConvId(''), {message: 'message2'}),
+        () => waitAsync(10 * 1000),
+        // turn meg inactive for 1 hour and look for notification email
         () => UC.sysclient.sys_call("sys/shard/time_travel", {
             object_id: UC.meg.account_id,
-            mk_time_action: 'account_activity_time',
-            time_interval: '7 days',
+            mk_time_action: 'abg_email_notif_send',
+            time_interval: '1 hour',
         }),
-        // this test isn't working right now, no email received
-        () => UC.meg.waitMail({}),
+        // meg receives an email that includes Conversation with Bob Marley and msg150...
+        () => UC.meg.waitMail({
+            subject: /unread messages in Fleep/,
+            body: /Bob Marley: 123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890.../,
+        }),
+        // set a 60 character conversation topic
+        () => client.api_call("api/conversation/store/" + client.getConvId(''), {topic: topic60}),
+        // send one random message and two messages with mentions plus some random text
+        () => client.api_call("api/message/store/" + client.getConvId(topic60), {message: 'message3'}),
+        () => client.api_call("api/message/store/" + client.getConvId(topic60), {message: '@' + UC.meg.info.fleep_address + ' message4'}),
+        () => client.api_call("api/message/store/" + client.getConvId(topic60), {message: '@' + UC.meg.info.fleep_address + ' message5'}),
+        () => waitAsync(10 * 1000),
+        // turn meg inactive for 2 days
+        () => UC.sysclient.sys_call("sys/shard/time_travel", {
+            object_id: UC.meg.account_id,
+            mk_time_action: 'abg_email_notif_send',
+            time_interval: '2 days',
+        }),
+        // meg receives an email that includes the first 50 characters of the topic plus ... plus message4
+        () => UC.meg.waitMail({
+            subject: /unread messages in Fleep/,
+            body: /09876543210987654321098765432109876543210987654321.../,
+        }),
     ]);
 });
